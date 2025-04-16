8e7a6a4e38f284b052511afd8bd5508f2316bc01
Blacklorren <quentin.herfeld@gmail.com>
2025-04-17 00:35:48 +0200

    Delete Scripts directory

--- Scripts/Simulation/Events/IMatchFinalizer.cs
+++
@@ -1,9 +0,0 @@
-using HandballManager.Simulation.Core.MatchData;
-
-namespace HandballManager.Simulation.Events
-{
-    public interface IMatchFinalizer
-    {
-        void UpdateTimers(MatchState state, float deltaTime, IMatchEventHandler eventHandler);
-    }
-}
